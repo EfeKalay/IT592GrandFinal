8754e8e2967d032fc7b6e64d9f4e8a99ff6663d4
Efe Kalay <efekalay@Efe-MacBook-Pro.local>
2021-06-25 23:47:29 +0300

    Project Start Commit

---
+++ ipamdemo/src/main/java/edu/sabanciuniv/ipamdemo/IpamdemoApplication.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.ipamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpamdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpamdemoApplication.class, args);
+	}
+
+}
---
+++ ipamdemo/src/test/java/edu/sabanciuniv/ipamdemo/IpamdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.ipamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpamdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
